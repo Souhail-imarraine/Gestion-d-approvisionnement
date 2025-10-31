1daad421be9f8a165876f0473be8e9d4010ee8d6
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-10-31 19:43:04 +0100

    SCRUM-2 Configuration Base de Données MySQL

--- src/main/java/com/tricol/stock/service/FournisseurService.java
+++ src/main/java/com/tricol/stock/service/FournisseurService.java
@@ -1,58 +1,17 @@
-package com.tricol.stock.service;
-
+package com.tricol.stock.service.impl;
 import com.tricol.stock.dto.FournisseurDTO;
-import com.tricol.stock.entity.Fournisseur;
-import com.tricol.stock.exception.ResourceNotFoundException;
-import com.tricol.stock.mapper.FournisseurMapper;
-import com.tricol.stock.repository.FournisseurRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class FournisseurService {
+public interface FournisseurImpl {
 
-    private final FournisseurRepository repository;
-    private final FournisseurMapper mapper;
+    FournisseurDTO create(FournisseurDTO dto);
 
-    public List<FournisseurDTO> findAll() {
-        return mapper.toDTOList(repository.findAll());
-    }
+    FournisseurDTO update(Long id, FournisseurDTO dto);
 
-    public FournisseurDTO findById(Long id) {
-        Fournisseur fournisseur = repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Fournisseur non trouvé avec l'ID: " + id));
-        return mapper.toDTO(fournisseur);
-    }
+    FournisseurDTO findById(Long id);
 
-    public FournisseurDTO create(FournisseurDTO dto) {
-        Fournisseur fournisseur = mapper.toEntity(dto);
-        Fournisseur saved = repository.save(fournisseur);
-        return mapper.toDTO(saved);
-    }
+    List<FournisseurDTO> findAll();
 
-    public FournisseurDTO update(Long id, FournisseurDTO dto) {
-        Fournisseur existing = repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Fournisseur non trouvé avec l'ID: " + id));
-
-        existing.setRaisonSociale(dto.getRaisonSociale());
-        existing.setAdresse(dto.getAdresse());
-        existing.setVille(dto.getVille());
-        existing.setPersonneContact(dto.getPersonneContact());
-        existing.setEmail(dto.getEmail());
-        existing.setTelephone(dto.getTelephone());
-        existing.setIce(dto.getIce());
-
-        Fournisseur updated = repository.save(existing);
-        return mapper.toDTO(updated);
-    }
-
-    public void delete(Long id) {
-        if (!repository.existsById(id)) {
-            throw new ResourceNotFoundException("Fournisseur non trouvé avec l'ID: " + id);
-        }
-        repository.deleteById(id);
-    }
+    void delete(Long id);
 }
