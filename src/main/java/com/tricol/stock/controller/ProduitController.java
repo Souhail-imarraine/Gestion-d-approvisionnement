158f6833070172eb6f043fb23359b72387c35da3
Mohammed <mkaroumi123@gmail.com>
2025-11-08 19:57:41 +0100

    Corriger le bug de suppression de produit avec contrainte de commande

--- src/main/java/com/tricol/stock/controller/ProduitController.java
+++ src/main/java/com/tricol/stock/controller/ProduitController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -39,9 +40,11 @@ public class ProduitController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<HashMap<String, String>> delete(@PathVariable Long id) {
         service.delete(id);
-        return ResponseEntity.noContent().build();
+        HashMap<String, String> message = new HashMap<>();
+        message.put("message", "Produit supprime avec succes");
+        return ResponseEntity.ok(message);
     }
 
     @GetMapping("/alertes")
--- src/main/java/com/tricol/stock/service/impl/ProduitServiceImp.java
+++ src/main/java/com/tricol/stock/service/impl/ProduitServiceImp.java
@@ -5,43 +5,44 @@ import com.tricol.stock.entity.Produit;
 import com.tricol.stock.exception.DuplicateReferenceException;
 import com.tricol.stock.exception.ResourceNotFoundException;
 import com.tricol.stock.mapper.ProduitMapper;
+import com.tricol.stock.repository.LigneCommandeRepository;
 import com.tricol.stock.repository.ProduitRepository;
 import com.tricol.stock.service.ProduitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Primary
 public class ProduitServiceImp implements ProduitService {
-    private final ProduitRepository repository;
-    private final ProduitMapper ProduitMapper;
+    private final ProduitRepository produitRepository;
+    private final ProduitMapper produitMapper;
+    private final LigneCommandeRepository ligneCommandeRepository;
 
     public List<ProduitDTO> findAll() {
-        return ProduitMapper.toDTOList(repository.findAll());
+        return produitMapper.toDTOList(produitRepository.findAll());
     }
 
     public ProduitDTO findById(Long id) {
-        Produit produit = repository.findById(id)
+        Produit produit = produitRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Produit non trouvé avec l'ID: " + id));
-        return ProduitMapper.toDTO(produit);
+        return produitMapper.toDTO(produit);
     }
 
     public ProduitDTO create(ProduitDTO dto) {
-        if(dto.getReference() != null && repository.existsByreference(dto.getReference())){
+        if(dto.getReference() != null && produitRepository.existsByreference(dto.getReference())){
             throw new DuplicateReferenceException("La référence" +dto.getReference()+" existe déjà");
         }
-        Produit produit = ProduitMapper.toEntity(dto);
-        Produit saved = repository.save(produit);
-        return ProduitMapper.toDTO(saved);
+        Produit produit = produitMapper.toEntity(dto);
+        Produit saved = produitRepository.save(produit);
+        return produitMapper.toDTO(saved);
     }
 
     public ProduitDTO update(Long id, ProduitDTO dto) {
-        Produit existing = repository.findById(id)
+        Produit existing = produitRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Produit non trouvé avec l'ID: " + id));
 
         existing.setReference(dto.getReference());
@@ -53,18 +54,21 @@ public class ProduitServiceImp implements ProduitService {
         existing.setPointCommande(dto.getPointCommande());
         existing.setUniteMesure(dto.getUniteMesure());
 
-        Produit updated = repository.save(existing);
-        return ProduitMapper.toDTO(updated);
+        Produit updated = produitRepository.save(existing);
+        return produitMapper.toDTO(updated);
     }
 
     public void delete(Long id) {
-        if (!repository.existsById(id)) {
+        if (!produitRepository.existsById(id)) {
             throw new ResourceNotFoundException("Produit non trouvé avec l'ID: " + id);
         }
-        repository.deleteById(id);
+        if (!ligneCommandeRepository.findByProduitId(id).isEmpty()){
+            throw new IllegalArgumentException("Vous ne pouvez pas supprimer un produit qui existe dans une commande");
+        }
+        produitRepository.deleteById(id);
     }
 
     public List<ProduitDTO> findProduitsEnAlerte() {
-        return ProduitMapper.toDTOList(repository.findProduitsEnAlerte());
+        return produitMapper.toDTOList(produitRepository.findProduitsEnAlerte());
     }
 }
