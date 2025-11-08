88a1238dd69581513f0d564bb30e8d479fb64a0e
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-11-08 12:00:00 +0100

    feat: Créer repositories pour LotStock et MouvementStock avec méthodes FIFO

---
+++ src/main/java/com/tricol/stock/repository/MouvementStockRepository.java
@@ -0,0 +1,20 @@
+package com.tricol.stock.repository;
+
+import com.tricol.stock.entity.MouvementStock;
+import com.tricol.stock.enums.TypeMouvement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MouvementStockRepository extends JpaRepository<MouvementStock, Long> {
+    
+    List<MouvementStock> findByProduitId(Long produitId);
+    
+    List<MouvementStock> findByLotId(Long lotId);
+    
+    List<MouvementStock> findByTypeMouvement(TypeMouvement type);
+    
+    List<MouvementStock> findByProduitIdOrderByDateMouvementDesc(Long produitId);
+}
