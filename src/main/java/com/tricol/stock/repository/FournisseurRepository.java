ab1d67a50916e354ec3a1b17ae1e64669b6852ab
Karroumi-Mohamed <mkaroumi123@gmail.com>
2025-11-08 16:59:20 +0100

    add validation fourniseur

--- src/main/java/com/tricol/stock/repository/FournisseurRepository.java
+++ src/main/java/com/tricol/stock/repository/FournisseurRepository.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
 
     Optional<Fournisseur> findByIce(String ice);
-
+    boolean existsByIce(String ice);
     List<Fournisseur> findByVille(String ville);
+    boolean existsByEmail (String email);
     List<Fournisseur> findByRaisonSocialeContainingIgnoreCase(String nom);
 }
