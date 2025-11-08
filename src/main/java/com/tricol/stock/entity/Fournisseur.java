49284c325f397fdbf25f6772505eed305cf5f604
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-11-08 12:00:00 +0100

    SCRUM-5 Créer Entity Fournisseur

--- src/main/java/com/tricol/stock/entity/Fournisseur.java
+++ src/main/java/com/tricol/stock/entity/Fournisseur.java
@@ -10,29 +10,30 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Fournisseur {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false, length = 100)
     private String raisonSociale;
-    
+
     @Column(length = 200)
     private String adresse;
-    
+
     @Column(length = 50)
     private String ville;
-    
+
     @Column(length = 100)
     private String personneContact;
-    
+
     @Column(length = 100)
     private String email;
-    
+
     @Column(length = 20)
     private String telephone;
-    
+
     @Column(unique = true, length = 15)
     private String ice;
+
 }
